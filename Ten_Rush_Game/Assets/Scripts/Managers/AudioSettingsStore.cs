16d174f2bed993a64add7e3474da0a76d002afac
Pudding <gkscksgma14@gmail.com>
2026-09-11 18:11:31 +0900

    fix: BGM/SFX 기본 볼륨을 50%로 변경

--- Ten_Rush_Game/Assets/Scripts/Managers/AudioSettingsStore.cs
+++ Ten_Rush_Game/Assets/Scripts/Managers/AudioSettingsStore.cs
@@ -25,7 +25,7 @@ namespace TenRush.Managers
 
         public static float BgmVolume
         {
-            get => PlayerPrefs.GetFloat(BgmVolumeKey, 0.7f);
+            get => PlayerPrefs.GetFloat(BgmVolumeKey, 0.5f);
             set
             {
                 PlayerPrefs.SetFloat(BgmVolumeKey, Mathf.Clamp01(value));
@@ -45,7 +45,7 @@ namespace TenRush.Managers
 
         public static float SfxVolume
         {
-            get => PlayerPrefs.GetFloat(SfxVolumeKey, 1f);
+            get => PlayerPrefs.GetFloat(SfxVolumeKey, 0.5f);
             set
             {
                 PlayerPrefs.SetFloat(SfxVolumeKey, Mathf.Clamp01(value));
